fb8d14d17d2b1c672d99e6bf920fc7ae210a7955
Kevin <gmtip00@gmail.com>
2024-05-12 22:34:59 -0500

    vs 3

--- test2.js
+++ test2.js
@@ -1,2 +1,11 @@
 //archivo nuevo 2
 //archivo nuevo 3 DESDE GIT
+// Función para restar dos números
+function restar(a, b) {
+    return a - b;
+}
+
+// Función para multiplicar dos números
+function multiplicar(a, b) {
+    return a * b;
+}
